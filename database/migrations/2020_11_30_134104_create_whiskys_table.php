0d1019a99d0914d48b76d52ba7a699cd0bc5559a
kenta.koizumi <koizumi.6626@gmail.com>
2021-01-14 09:47:22 +0000

    alter file name

--- database/migrations/2020_11_30_134104_create_whiskys_table.php
+++ database/migrations/2020_11_30_134104_create_whiskys_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWhiskiesTable extends Migration
+class CreateWhiskysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateWhiskiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('whiskies', function (Blueprint $table) {
+        Schema::create('whiskys', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('whisky_name');
             $table->string('maker');
