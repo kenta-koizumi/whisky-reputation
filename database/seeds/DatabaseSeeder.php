0a2e8643c1e81d6f0787403297e171bab490b42a
kenta.koizumi <koizumi.6626@gmail.com>
2021-01-17 05:24:26 +0000

    fix databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call([AftertastesTableSeeder::class]);
+        $this->call([ColorsTableSeeder::class]);
+        $this->call([SmellsTableSeeder::class]);
+        $this->call([TasteTableSeeder::class]);
+        $this->call([WaysTableSeeder::class]);
+        $this->call([WhisiesTableSeeder::class]);
     }
 }
